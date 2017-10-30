3a763545b6153c87a8e9517583f0ad90585786bc
dinhoarakaki <dinhoarakaki@gmail.com>
2017-10-30 00:41:06 -0300

    parece estar funcionando

--- js/home.js
+++ js/home.js
@@ -1,19 +1,28 @@
-var page_data_controller = new Vue({
-    el:'#page_data_controller',
-    data:{
-        form_vars:{
-            menu_ativo	    : ''
-        }
-    },
+// var page_data_controller = new Vue({
+//     el:'#page_data_controller',
+//     data:{
+//         form_vars:{
+//             menu_ativo	    : ''
+//         }
+//     },
+//
+//     methods: {
+//         open_file: function (file_name) {
+//             $.ajax({url: "/html/"+file_name, cache: false, async: false, success: function(result){
+//                 $('.page_data_content').html(result);
+//             }});
+//         },
+//     },
+//     created: function () {
+//         //this.search_menu('index');
+//     }
+// });
+function open_file(file_name){
+    $.ajax({url: "/html/"+file_name, cache: false, async: false, success: function(result){
+        $('.page_data_content').html(result);
+    }});
+}
 
-    methods: {
-        open_file: function (file_name) {
-            $.ajax({url: "/html/"+file_name, success: function(result){
-                $('.page_data_content').html(result);
-            }});
-        },
-    },
-    created: function () {
-        //this.search_menu('index');
-    }
-});
+open_file('home.html');
+
+
