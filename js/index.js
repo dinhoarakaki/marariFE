573655d3118f46ef4d4d34817d23cb72cf538f61
Matheus Vilvert <matheusvilvert@gmail.com>
2017-11-27 18:41:05 -0200

    tentando arrumar login

--- js/index.js
+++ js/index.js
@@ -28,13 +28,31 @@ var myChartB ='';
 var myChartL = '';
 var coresFundo = [];
 var coresBorda =[];
+var usuarioLogado ='';
 
-function open_file(file_name){
+/*function open_file(file_name){
     $.ajax({url: "/html/"+file_name, cache: false, async: false, success: function(result){
         $('.page_data_content').html(result);
         $('#navbarResponsive').removeClass('show');
         $('#toTop').click();
     }});
+}*/
+var now = new Date();
+function open_file(file_name){
+    var agora = now.getTime();
+    var tempo_permitido =  window.sessionStorage.getItem('tempo_sessao');
+    tempo_permitido = parseInt(tempo_permitido) + parseInt(300000);
+    //console.log('Tempo permitido: '+tempo_permitido+' || Agora: '+agora);
+    if( tempo_permitido >= agora ){
+        window.sessionStorage.setItem('tempo_sessao', agora);
+        //console.log('novo local storage: '+sessionStorage.getItem('tempo_sessao'));
+        $.ajax({url: "/html/"+file_name, cache: false, async: false, success: function(result){
+            $('.page_data_content').html(result);
+            $('#navbarResponsive').removeClass('show');
+        }});
+    }else{
+        window.location = "../login.html";
+    }
 }
 
 open_file('home.html');
